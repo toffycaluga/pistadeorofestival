52eaae0bd93e21ea68aa62d0aeb5740d0f0776be
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2022-08-30 22:24:46 -0400

    paginas proximamente y 404 listas

--- src/Components/Festival/Festival.jsx
+++ src/Components/Festival/Festival.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './Festival.css'
 import next from './../../assets/img/icons/next.png'
 
+
 function Festival() {
   return (
-    <div className="Festival">
+    <div className="festival">
         <img src={next} alt="" />
     </div>
   )
--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './NotFound.css'
 import i404 from './../../assets/img/icons/404.png'
+
 function NotFound() {
   return (
     <div className="not-found">
+
         <img src={i404} alt="" />
+
     </div>
   )
 }
