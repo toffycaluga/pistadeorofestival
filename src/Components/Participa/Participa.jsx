e8c6c3adf03fa299c9b5d5f90b07ca2bab7c7024
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2022-09-21 16:42:34 -0300

    postulaciones cerradas

--- src/Components/Participa/Participa.jsx
+++ src/Components/Participa/Participa.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Formulario from './Formulario'
+// import Formulario from './Formulario'
 import './Participa.css'
 import logo_festival from './../../assets/img/icons/logo_festival.png'
 import PostulcacionCerrada from './PostulcacionCerrada'
@@ -12,11 +12,11 @@ function Participa() {
         <div className="container-form">
             <div className="info-form">
                 <p></p>
-                <p>Se parte de este gran evento efectuado por el Sindicato de Artists Circenses de Chile.</p>
+                <p><b>Se parte de este gran evento efectuado por el Sindicato de Artists Circenses de Chile.</b></p>
                 <img src={logo_festival} alt="" />
             </div>
             {/* <Formulario/> */}
-            <PostulcacionCerrada></PostulcacionCerrada>
+            <PostulcacionCerrada/>
         </div>
 
     </div>
--- src/Components/Participa/PostulcacionCerrada.jsx
+++ src/Components/Participa/PostulcacionCerrada.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 
 function PostulcacionCerrada() {
   return (
-    <div className='participate'>
+    <div className='postulacion cerrada info-form'>
                 <h2><b>Postulaciones Cerradas</b></h2>
-        <div className="container-form">
-            <div className="info-form">
                 <p></p>
                 <p>Lamentablamente el proceso de postulación ya cerro.</p>
                 <p>pero te invitamos a seguir mostrando esfuerzo y amor al circo,
                     te esperamos con ansias el próximo año !
                 </p>
-
-                
-            </div>
-            <Formulario/>
-        </div>
         </div>
   )
 }
