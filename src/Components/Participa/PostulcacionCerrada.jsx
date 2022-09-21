4bad91690aab2f1a04edf9836f62c5744857c522
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2022-09-21 16:24:01 -0300

    postulaciones cerradas

---
+++ src/Components/Participa/PostulcacionCerrada.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function PostulcacionCerrada() {
+  return (
+    <div className='participate'>
+                <h2><b>Postulaciones Cerradas</b></h2>
+        <div className="container-form">
+            <div className="info-form">
+                <p></p>
+                <p>Lamentablamente el proceso de postulación ya cerro.</p>
+                <p>pero te invitamos a seguir mostrando esfuerzo y amor al circo,
+                    te esperamos con ansias el próximo año !
+                </p>
+
+                
+            </div>
+            <Formulario/>
+        </div>
+        </div>
+  )
+}
+
+export default PostulcacionCerrada
