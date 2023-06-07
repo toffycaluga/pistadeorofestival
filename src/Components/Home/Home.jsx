162ca32a8e51fe8ee2ba2d4608dbba6f164550c6
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-07 19:09:34 -0300

    editando metadatos

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,40 +5,41 @@ import instagram from './../../assets/img/home/instagram.png'
 import jurado from './../../assets/img/home/jurado.png'
 import youtube from './../../assets/img/home/youtube.png'
 import './Home.css'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 
 function Portada() {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="home">
-        <Carousel/>
+      <Carousel />
 
-
-        <div className="home-container">
-          <div className="home-content">
-            <img src={youtube} alt="canal del festival" />
-            </div>
-          <div className="home-content">
-            <a href="https://ticketplus.cl/events/festival-internacional-de-circo-en-chile">
+      <h1>Festival Internacional de circo Chile</h1>
+      <h2>Pista de Oro</h2>
+      <div className="home-container">
+        <div className="home-content">
+          <img src={youtube} alt="canal del festival" />
+        </div>
+        <div className="home-content">
+          <a href="https://ticketplus.cl/events/festival-internacional-de-circo-en-chile">
             <img src={boletos} alt="boletos" />
-            </a>
-            </div>
+          </a>
         </div>
-        <div className="home-container">
+      </div>
+      <div className="home-container">
 
-          <div className="home-content">
-            <img src={jurado} alt="jurado" onClick={()=>navigate('/jurado')}/>
-            </div>
-          <div className="home-content">
-            <a href="https://instagram.com/festivalpistadeoro">
-
-            <img src={instagram} alt="fotos del festival"  />
-            </a>
-            </div>
+        <div className="home-content">
+          <img src={jurado} alt="jurado" onClick={() => navigate('/jurado')} />
         </div>
-      
+        <div className="home-content">
+          <a href="https://instagram.com/festivalpistadeoro">
+
+            <img src={instagram} alt="fotos del festival" />
+          </a>
+        </div>
+      </div>
+
     </div>
   )
 }
